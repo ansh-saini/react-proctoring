515fb1293f8d6a3ee2044bcdad8f5e27e4c0b762
Ansh Saini <anshsaini625@gmail.com>
2023-02-12 14:54:33 +0530

    feat: setup vite to build project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+const { EsLinter, linterPlugin } = EsLint
+import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'react-proctoring',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-proctoring.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react-device-detect', ...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}))
