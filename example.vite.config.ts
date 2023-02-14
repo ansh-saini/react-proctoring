68a5cf8ae63ff3c6470fdaa50feb032ee74e2305
Ansh Saini <anshsaini625@gmail.com>
2023-02-14 09:43:31 +0530

    feat: setup example build script

---
+++ example.vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+    rollupOptions: {},
+  },
+})
